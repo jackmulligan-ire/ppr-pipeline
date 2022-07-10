2fe80c97a2f614f673617f1720b64b22b50f202a
Jack Mulligan <mulligja1@gmail.com>
2022-07-10 09:47:33 +0100

    Wrote method to request data from ppr-website, then upload to the S3

--- ppr_pipeline/__main__.py
+++ ppr_pipeline/__main__.py
@@ -1,5 +1,4 @@
 from ppr_pipeline.scrapers import PPR_Hist_Scraper
 
 if __name__ == "__main__":
-    PPR_Hist_Scraper.request_zipfile()
-    PPR_Hist_Scraper.transform_zipfile_to_csv()
+    PPR_Hist_Scraper.upload_hist_data()
--- ppr_pipeline/scrapers.py
+++ ppr_pipeline/scrapers.py
@@ -1,16 +1,32 @@
+# Python libs
 import requests
 from zipfile import ZipFile
+from io import BytesIO
+
+# Third-party libs
+import boto3
 
 class PPR_Hist_Scraper():
     @classmethod
-    def request_zipfile(cls):
+    def upload_hist_data(cls):
+        response_content = PPR_Hist_Scraper._request_zipfile()
+        csv_data = PPR_Hist_Scraper._extract_csv_data(response_content)
+        PPR_Hist_Scraper._upload_csv_to_s3(csv_data)
+
+    @classmethod
+    def _request_zipfile(cls):
         response = requests.get(
             "https://www.propertypriceregister.ie/website/npsra/ppr/npsra-ppr.nsf/Downloads/PPR-ALL.zip/$FILE/PPR-ALL.zip",
             verify=False)
-        with open("tmp/ppr-hist-data.zip", 'wb') as zip_file:
-            zip_file.write(response.content)
+        return response.content
 
     @classmethod
-    def transform_zipfile_to_csv(cls):
-        with ZipFile('tmp/ppr-hist-data.zip', 'r') as zipObj:
-            zipObj.extractall('tmp')
+    def _extract_csv_data(cls, response_content):
+        with ZipFile(BytesIO(response_content)) as zipObj:
+            csv_data = zipObj.read('PPR-ALL.csv')
+            return BytesIO(csv_data)
+
+    @classmethod
+    def _upload_csv_to_s3(cls, csv_data):
+        s3_client = boto3.client('s3')
+        s3_client.upload_fileobj(csv_data, 'ppr-pipeline', 'PPR-ALL.csv')
