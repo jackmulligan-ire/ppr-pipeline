c7b9d47ff8a5e0b79ccca600505dbaf9d1adce54
Jack Mulligan <mulligja1@gmail.com>
2022-07-23 22:22:55 +0100

    Wrote private method to inject ppr data into the staging database

--- ppr_pipeline/injectors/injectors.py
+++ ppr_pipeline/injectors/injectors.py
@@ -1,29 +1,56 @@
 import csv
+import os
 
 # Third-party libs
 import boto3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 # App modules
 import ppr_pipeline
+from ppr_pipeline.injectors.orm_classes import Property_Transaction_Staging
 
 class PPR_Hist_Injector():
     @classmethod
     def inject_ppr_data(cls):
         cls._get_all_from_s3()
         ppr_data = cls._generate_iter_from_csv('tmp/PPR-ALL.csv')
+        cls._inject_data_to_staging(ppr_data)
 
     @classmethod
     def _get_all_from_s3(cls):
         s3 = boto3.client('s3')
         s3.download_file(ppr_pipeline.BUCKET_NAME, ppr_pipeline.ALL_OBJECT_NAME, 'tmp/PPR-ALL.csv')
 
-        '''
-        with open('tmp/PPR-ALL.csv', 'r') as csv_file:
-            reader = csv.reader(csv_file)
-            return reader
-        '''
     @classmethod
     def _generate_iter_from_csv(cls, filename):
         with open(filename) as csv_file:
             reader = csv.DictReader(csv_file)
             return [row for row in reader]
+
+    @classmethod
+    def _inject_data_to_staging(cls, ppr_rows):
+        USER = os.environ.get('DB_USER')
+        PASSWORD = os.environ.get('DB_PASSWORD')
+        HOST = os.environ.get('DB_HOST')
+        PORT = os.environ.get('DB_PORT')
+        NAME = os.environ.get('DB_NAME')
+
+        db_string = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"
+        engine = create_engine(db_string)
+        
+        with Session(engine) as session:
+            for data in ppr_rows:
+                prop_trans = Property_Transaction_Staging(
+                    address = data['Address'],
+                    county = data['County'],
+                    eircode = data['Eircode'],
+                    price = data['Price (�)'],
+                    not_full_market_price = data['Not Full Market Price'],
+                    vat_exclusive = data['VAT Exclusive'],
+                    description_of_property = data['Description of Property'],
+                    property_size_description = data['Property Size Description'],
+                    date_of_sale = data['Date of Sale (dd/mm/yyyy)']
+                )
+                session.add(prop_trans)
+                session.commit()
--- test/test_injectors.py
+++ test/test_injectors.py
@@ -1,5 +1,6 @@
 import unittest
-from unittest.mock import patch
+import os
+from unittest.mock import patch, call
 from ppr_pipeline.injectors.injectors import PPR_Hist_Injector
 
 class TestInjectors(unittest.TestCase):
@@ -21,7 +22,86 @@ class TestInjectors(unittest.TestCase):
             'Description of Property' : 'Second-Hand Dwelling house /Apartment',
             'Property Size Description': ''
         }]
+
         self.assertEqual(PPR_iter_result, PPR_iter_data)
+
+    def test_create_engine(self):
+        USER = os.environ.get('DB_USER')
+        PASSWORD = os.environ.get('DB_PASSWORD')
+        HOST = os.environ.get('DB_HOST')
+        PORT = os.environ.get('DB_PORT')
+        DBNAME = os.environ.get('DB_NAME')
+
+        PPR_iter_data = [{
+            'Date of Sale (dd/mm/yyyy)': '01/01/2010',
+            'Address' : '5 Braemor Drive, Churchtown, Co.Dublin',
+            'County' : 'Dublin',
+            'Eircode' : '',
+            'Price (�)' : '�343,000.00',
+            'Not Full Market Price' : 'No',
+            'VAT Exclusive' : 'No',
+            'Description of Property' : 'Second-Hand Dwelling house /Apartment',
+            'Property Size Description': ''
+        }]
+
+        db_string = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
+
+        with patch('ppr_pipeline.injectors.injectors.create_engine') as mocked_engine:
+            PPR_Hist_Injector._inject_data_to_staging(PPR_iter_data)
+            mocked_engine.assert_called_once_with(db_string)
+
+    def test_staging_orm(self):
+        PPR_iter_data = [
+            {
+            'Date of Sale (dd/mm/yyyy)': '01/01/2010',
+            'Address' : '5 Braemor Drive, Churchtown, Co.Dublin',
+            'County' : 'Dublin',
+            'Eircode' : '',
+            'Price (�)' : '�343,000.00',
+            'Not Full Market Price' : 'No',
+            'VAT Exclusive' : 'No',
+            'Description of Property' : 'Second-Hand Dwelling house /Apartment',
+            'Property Size Description': ''
+            },
+            {
+            'Date of Sale (dd/mm/yyyy)': '01/01/2010',
+            'Address' : '5 Braemor Drive, Churchtown, Co.Dublin',
+            'County' : 'Dublin',
+            'Eircode' : '',
+            'Price (�)' : '�343,000.00',
+            'Not Full Market Price' : 'No',
+            'VAT Exclusive' : 'No',
+            'Description of Property' : 'Second-Hand Dwelling house /Apartment',
+            'Property Size Description': ''
+            },
+        ]
+
+        with patch('ppr_pipeline.injectors.injectors.Property_Transaction_Staging') as mocked_orm:
+            PPR_Hist_Injector._inject_data_to_staging(PPR_iter_data)
+            mocked_orm.assert_has_calls([
+                call(
+                    address = PPR_iter_data[0]['Address'],
+                    county = PPR_iter_data[0]['County'],
+                    eircode = PPR_iter_data[0]['Eircode'],
+                    price = PPR_iter_data[0]['Price (�)'],
+                    not_full_market_price = PPR_iter_data[0]['Not Full Market Price'],
+                    vat_exclusive = PPR_iter_data[0]['VAT Exclusive'],
+                    description_of_property = PPR_iter_data[0]['Description of Property'],
+                    property_size_description = PPR_iter_data[0]['Property Size Description'],
+                    date_of_sale = PPR_iter_data[0]['Date of Sale (dd/mm/yyyy)']
+                ),
+                call(
+                    address = PPR_iter_data[1]['Address'],
+                    county = PPR_iter_data[1]['County'],
+                    eircode = PPR_iter_data[1]['Eircode'],
+                    price = PPR_iter_data[1]['Price (�)'],
+                    not_full_market_price = PPR_iter_data[1]['Not Full Market Price'],
+                    vat_exclusive = PPR_iter_data[1]['VAT Exclusive'],
+                    description_of_property = PPR_iter_data[1]['Description of Property'],
+                    property_size_description = PPR_iter_data[1]['Property Size Description'],
+                    date_of_sale = PPR_iter_data[1]['Date of Sale (dd/mm/yyyy)']
+                )
+            ])
 
 if __name__ == '__main__':
     unittest.main()
