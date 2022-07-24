33be43d6c78d2ada5a4611c72da6b58289dd51c9
Jack Mulligan <mulligja1@gmail.com>
2022-07-24 12:06:50 +0100

    Wrote entrypoints from cmd line for scraping all data, injecting all data

--- ppr_pipeline/__main__.py
+++ ppr_pipeline/__main__.py
@@ -1,6 +1,19 @@
+import sys
+
 from ppr_pipeline.scrapers.scrapers import PPR_Hist_Scraper
 from ppr_pipeline.injectors.injectors import PPR_Hist_Injector
 
+def main():
+    try:
+        cmd_line_arg = sys.argv[1]
+        if cmd_line_arg == "scrape_all":
+            PPR_Hist_Scraper.upload_hist_data()
+        elif cmd_line_arg == "inject_all":
+            PPR_Hist_Injector.inject_ppr_data()
+        else:
+            raise Exception
+    except:
+        raise SystemExit(f"Error. Usage: python -m ppr_pipeline [scrape_all|inject_all]")
+
 if __name__ == "__main__":
-    #PPR_Hist_Scraper.upload_hist_data()
-    PPR_Hist_Injector.inject_ppr_data()
+    main()
