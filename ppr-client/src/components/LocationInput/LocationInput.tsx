ebb5b24a6ba530c9d667da532c0b5ca143061326
Jack Mulligan <mulligja1@gmail.com>
2023-01-20 20:57:04 +0000

    Added grid around location component

--- ppr-client/src/components/LocationInput/LocationInput.tsx
+++ ppr-client/src/components/LocationInput/LocationInput.tsx
@@ -1,19 +1,28 @@
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 
 const locationOptions = ["Carlow", "Cavan", "Clare"];
 
 function LocationInput() {
   return (
-    <Autocomplete
-      multiple
-      limitTags={5}
-      disablePortal
-      options={locationOptions}
-      renderInput={(params) => (
-        <TextField {...params} label="Locations" helperText="Add a county" />
-      )}
-    />
+    <Grid container>
+      <Grid item xs={12} sm={6}>
+        <Autocomplete
+          multiple
+          limitTags={5}
+          disablePortal
+          options={locationOptions}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Locations"
+              helperText="Add a county"
+            />
+          )}
+        />
+      </Grid>
+    </Grid>
   );
 }
 
